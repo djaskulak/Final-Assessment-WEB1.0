6515b689185161c1d0dcf01c71e5a5ac9e34c50e
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2020-10-06 17:23:57 -0700

    finished functions in javascript file

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function changeSize() {
 	// get the font size:
 	const fontSize = inputSize.value + 'px'
 	// Set the style 
-	display.style.fontSize = fontSize
+    display.style.fontSize = fontSize
 	// Show the size
 	showSize.innerHTML = fontSize
 }
@@ -17,15 +17,12 @@ function changeSize() {
 // The select field for font
 const selectFont = document.querySelector('#select-font')
 const showFont = document.querySelector('#show-font')
-selectFont.addEventListener('input', replaceFont)
+selectFont.addEventListener('change', replaceFont)
 function replaceFont() {
-    const h1 = document.querySelector('#display') // Selects the h1 tag
-    
-    const selectFont = document.querySelector('#select-font')
-    let font_str = selectFont.value
-    console.log(font_str)
+    const fontSel = selectFont.value
   
-    h1.style['font-family'] = font_str
+    display.style.fontFamily = fontSel
+    showFont.innerHTML = fontSel
   }
 
 // The input field for text color
@@ -33,21 +30,29 @@ const inputColor = document.querySelector('#input-color')
 const showColor = document.querySelector('#show-color')
 inputColor.addEventListener('input', fontColor)
 function fontColor() {
-    const h1 = document.querySelector('#display')
-    
-    const color_input = document.querySelector('#input-color')
-    let color_str = color_input.value
-    console.log(color_str)
+    const fontColor = inputColor.value
   
-    h1.style.color = color_str
+    display.style.color = fontColor
+    showColor.innerHTML = fontColor
   }
 
 // The input field for background color
 const inputBGColor = document.querySelector('#input-bg-color')
 const showBGColor = document.querySelector('#show-bg-color')
 inputBGColor.addEventListener('input', BGColor)
+function BGColor() {
+    const BGColorInput = inputBGColor.value
+  
+    display.style.backgroundColor = BGColorInput
+    showBGColor.innerHTML = BGColorInput
+}
 
 // The input field for changing the text
-const inputBGColor = document.querySelector('#enter-text')
-const showBGColor = document.querySelector('#show-bg-color')
-inputBGColor.addEventListener('input', display)
+const inputText = document.querySelector('#enter-text')
+inputText.addEventListener('input', displayText)
+function displayText() {
+    const textInput = document.querySelector('#enter-text')
+    let inputStr = textInput.value
+
+    display.innerHTML = `${inputStr}`
+}
